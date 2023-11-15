2856635e2bde0451120deec936193a5c89a9f7b3
Kwame Porter Robinson <kporterrobinson@gmail.com>
2023-11-15 17:35:32 -0500

    (wip) makes fetch and archive a bit smarter

--- fetch_and_archive.sh
+++ fetch_and_archive.sh
@@ -2,16 +2,24 @@
 
 # Define directories
 export REPO_DIR=$PWD
-mkdir -p $REPO_DIR/models  # pre-trained models
-mkdir -p $REPO_DIR/models/fastmetro_checkpoint  # model checkpoints
-mkdir -p $REPO_DIR/datasets  # datasets
+MODELS_DIR=$REPO_DIR/models/hrnet
 
-# Set blob URL
-BLOB='https://datarelease.blob.core.windows.net/metro'
+if [ ! -d "$MODELS_DIR" ]; then
+    mkdir -p $REPO_DIR/models/hrnet  # pre-trained models
+    mkdir -p $REPO_DIR/models/fastmetro_checkpoint  # model checkpoints
+    mkdir -p $REPO_DIR/datasets  # datasets
 
-# Download the ImageNet pre-trained HRNet models 
-wget -nc $BLOB/models/hrnetv2_w64_imagenet_pretrained.pth -O $REPO_DIR/models/hrnet/hrnetv2_w64_imagenet_pretrained.pth
-wget -nc $BLOB/models/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100.yaml -O $REPO_DIR/models/hrnet/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100.yaml
+    # Set blob URL
+    BLOB='https://datarelease.blob.core.windows.net/metro'
 
-# Download FastMETRO-L-H64 Model
-wget --no-check-certificate 'https://docs.google.com/uc?id=1WU6q27SV7YNGCSBLypB5IGFVWMnL26io' -O $REPO_DIR/models/fastmetro_checkpoint
+    # Download the ImageNet pre-trained HRNet models 
+    wget -nc $BLOB/models/hrnetv2_w64_imagenet_pretrained.pth -O $REPO_DIR/models/hrnet/hrnetv2_w64_imagenet_pretrained.pth
+    wget -nc $BLOB/models/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100.yaml -O $REPO_DIR/models/hrnet/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100.yaml
+
+    # Download FastMETRO-L-H64 Model
+    wget --no-check-certificate 'https://docs.google.com/uc?id=1WU6q27SV7YNGCSBLypB5IGFVWMnL26io' -O $REPO_DIR/models/fastmetro_checkpoint
+
+    echo "Models downloaded successfully."
+else
+    echo "Models directory already exists. Skipping download."
+fi
