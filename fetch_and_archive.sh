9442346c441a00f9e6798cf8452d4f6222f66965
Kwame Porter Robinson <kporterrobinson@gmail.com>
2023-11-20 13:37:02 -0500

    (wip) fixes fetch from google drive, nested wget to confirm for us

--- fetch_and_archive.sh
+++ fetch_and_archive.sh
@@ -17,9 +17,12 @@ if [ ! -d "$MODELS_DIR" ]; then
     wget -nc $BLOB/models/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100.yaml -O $REPO_DIR/models/hrnet/cls_hrnet_w64_sgd_lr5e-2_wd1e-4_bs32_x100.yaml
 
     # Download FastMETRO-L-H64 Model
-    wget --no-check-certificate 'https://docs.google.com/uc?id=1WU6q27SV7YNGCSBLypB5IGFVWMnL26io' -O $REPO_DIR/models/fastmetro_checkpoint
+    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1WU6q27SV7YNGCSBLypB5IGFVWMnL26io' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1WU6q27SV7YNGCSBLypB5IGFVWMnL26io" -O $REPO_DIR/models/fastmetro_checkpoint/FastMETRO-L-H64_h36m_state_dict.bin && rm -rf /tmp/cookies.txt
 
     echo "Models downloaded successfully."
 else
     echo "Models directory already exists. Skipping download."
 fi
+
+
+
