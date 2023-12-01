052ab02596942f4f8cccdc4551389aa023c94ae9
Kwame Porter Robinson <kporterrobinson@gmail.com>
2023-12-01 17:14:49 -0500

    (feat) adds FastAPI'ed response that maintains .shape of tensor. Thanks Phind.com!

--- cog/predict.py
+++ cog/predict.py
@@ -8,6 +8,8 @@ from utils.default import update_config as hrnet_update_config
 from utils.default import _C as hrnet_config
 import logging
 
+import numpy as np
+from typing import Dict
 from cog import BasePredictor, Input, Path
 
 class Predictor(BasePredictor):
@@ -114,8 +116,8 @@ class Predictor(BasePredictor):
 
     # The arguments and types the model takes as input
     def predict(self,
-          image: Path = Input(description="Grayscale input image")
-    ) -> str:
+          image: Path = Input(description="Input .jpg image")
+    ) -> Dict[str, list]:
         """Run a single prediction on the model"""
         img = Image.open(image) # for easier copypasta
         img_tensor = self.transform(img)
@@ -128,4 +130,14 @@ class Predictor(BasePredictor):
         out = self.fastmetro(batch_imgs)
 
         logging.warn(f"{str(out['pred_3d_vertices_coarse'].shape)}")
-        return "we'd return 3d points in .xyz format"
+
+        # Detach tensors and convert to numpy arrays
+        pred_3d_vertices_intermediate = out['pred_3d_vertices_intermediate'].detach().cpu().numpy()
+        pred_cam = out['pred_cam'].detach().cpu().numpy()
+
+        # Convert numpy arrays to Python lists
+        pred_3d_vertices_intermediate_list = pred_3d_vertices_intermediate.tolist()
+        pred_cam_list = pred_cam.tolist()
+
+        # Return as dictionary
+        return {"pred_3d_vertices_intermediate": pred_3d_vertices_intermediate_list, "pred_cam": pred_cam_list}
