fdb78a1f1ddd8131c4917a49790e290023fd7857
Kwame Porter Robinson <kporterrobinson@gmail.com>
2023-12-01 17:22:48 -0500

    (wip) cleaning up repo, leaving only Cog package

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-# ----------------------------------------------------------------------------------------------
-# FastMETRO Official Code
-# Copyright (c) POSTECH Algorithmic Machine Intelligence Lab. (P-AMI Lab.) All Rights Reserved 
-# Licensed under the MIT license.
-# ----------------------------------------------------------------------------------------------
-
-__version__ = "0.1.0"
--- src/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# ----------------------------------------------------------------------------------------------
-# FastMETRO Official Code
-# Copyright (c) POSTECH Algorithmic Machine Intelligence Lab. (P-AMI Lab.) All Rights Reserved 
-# Licensed under the MIT license.
-# ----------------------------------------------------------------------------------------------
